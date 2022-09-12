3b793587829677ecdbb9707b83af66678da98650
julivoloshyn <yuliavoloshina02@gmail.com>
2022-09-12 22:43:05 +0300

    Initial commit

---
+++ src/test/java/com/clearsolutions/registeruser/RegisterUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clearsolutions.registeruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
