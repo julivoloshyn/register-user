d99883cc6be787a2c862bc3ac9b7bbef6d88f8e2
julivoloshyn <yuliavoloshina02@gmail.com>
2022-09-16 22:19:40 +0300

    Model and Repository are done. Database was connected successfully

---
+++ src/main/java/com/clearsolutions/registeruser/model/UserModel.java
@@ -0,0 +1,42 @@
+package com.clearsolutions.registeruser.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "Users1")
+public class UserModel {
+
+    @Id
+    //@GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(nullable = false, name = "user_id")
+    private String userId;
+
+    @Column(nullable = false, name = "email")
+    private String email;
+
+    @Column(nullable = false, name = "first_name")
+    private String firstName;
+
+    @Column(nullable = false, name = "last_name")
+    private String lastName;
+
+    @Column(nullable = false, name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+}
